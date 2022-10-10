da6cdc01f57f991ad2f99408e99ac9fe5498a700
jhonatanCarvalh0 <jhonatan.silva.4198@gmail.com>
2022-10-10 15:17:18 -0300

    Created functions (Get Valid Messages, Get Message By Id, Message Exists) && validation enhanced! && Insomnia archive with CRUD tests

--- index.js
+++ index.js
@@ -36,18 +36,41 @@ const messages = [
     },
 ];
 
+// FUNCTIONS
+
+const getValidMessages = () => messages.filter(Boolean);
+
+const getMessageById = id => getValidMessages().find(msg => msg.id === id);
+
+const messageExists = function (id, res) {
+    if (!getMessageById(id)) {
+        res.status(404).send('Message not found!');
+        return false;
+    }
+    return true;
+};
+
+const reqIsValid = function (message, res) {
+    if (!message || !message.text) {
+        res.status(400).send(
+            'Create Failed! Please check if {text} exists and is not empty and try again!',
+        );
+        return false;
+    }
+    return true;
+};
+
 // [GET] (All) - List all the messages
 app.get('/messages', (req, res) => {
-    res.send(messages.filter(Boolean)); // doesn't show null
+    res.send(getValidMessages()); // doesn't show null
 });
 
 // [GET] (Single) - List a especific message by ID
 app.get('/messages/:id', (req, res) => {
-    const { id } = req.params;
-    const message = messages[id - 1];
+    const id = +req.params.id;
+    const message = getMessageById(id);
 
-    if (!message) {
-        res.status(404).send('Message not found!');
+    if (!messageExists(id, res)) {
         return;
     }
 
@@ -58,14 +81,12 @@ app.get('/messages/:id', (req, res) => {
 app.post('/messages', (req, res) => {
     const message = req.body;
 
-    if (!message || !message.text) {
-        res.status(400).send(
-            'Create Failed! Please check if {text} exists and is not empty and try again!',
-        );
+    if (!reqIsValid(message, res)) {
         return;
     }
 
-    const id = messages.length + 1;
+    const lastId = messages[messages.length - 1];
+    const id = lastId.id + 1;
     message.id = id;
 
     messages.push(message);
@@ -75,55 +96,47 @@ app.post('/messages', (req, res) => {
 // [PUT] - Update a especific message by ID
 app.put('/messages/:id', (req, res) => {
     // GET: message by id who wants to update!
-    const { id } = req.params;
-    const message = messages[id - 1];
+    const id = +req.params.id;
+    const newMessage = req.body;
+    const message = getMessageById(id);
 
     // VALIDATION
-
-    if (id > messages.length) {
-        res.status(404).send('Message not found! Update Failed!');
+    if (!messageExists(id, res)) {
         return;
     }
 
-    if (!req.body.newText) {
-        res.status(400).send(
-            'Update failed! Please check that {newText} exists and is not empty and try again!',
-        );
+    if (!reqIsValid(message, res)) {
         return;
     }
 
-    // Get the new text from REQUEST BODY
-    const { newText } = req.body;
-
+    // GET NEW MESSAGE
+    const newText = newMessage.text;
     // UPDATE MESSAGE
-    const oldText = message.text;
+    const oldMessage = message.text;
     message.text = newText;
 
     res.send(
-        `[PUT] Update test: index = ${message.id}, message = {${oldText}} updated to {${newText}}`,
+        `[PUT] Update test: index = ${message.id}, message = {${oldMessage}} updated to {${newText}}`,
     );
 });
 
 // [DELETE] - Delete a especific message by ID
 app.delete('/messages/:id', (req, res) => {
-    const { id } = req.params;
-    const message = messages[id - 1];
-    const oldMessage = message.text;
+    const id = +req.params.id;
+    const message = getMessageById(id);
+    const oldMessage = message;
 
     // VALIDATION
-    if (id > messages.length) {
-        res.status(404).send('Message not found! Delete failed!');
+    if (!messageExists(id, res)) {
         return;
     }
 
     // DELETE MESSAGE
-    /* messages.splice(id - 1, 1);   // remove from array */
-
-    // DELETE MESSAGE
-    delete messages[id - 1]; // change for null
+    const indexOfMessage = messages.indexOf(message);
+    messages.splice(indexOfMessage, 1); // remove from array
 
     res.send(
-        `[DELETE] Delete test: index = ${message.id}, message = {${oldMessage}} removed!`,
+        `[DELETE] Delete test: index = ${message.id}, message = {${oldMessage.text}} removed!`,
     );
 });
 
